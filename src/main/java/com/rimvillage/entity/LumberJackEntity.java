73c92a0537fa436c39b6972f0fdc705df851e03f
Bistard <1015813038@qq.com>
2021-06-07 21:20:35 +0800

    init LumberJackEntity

---
+++ src/main/java/com/rimvillage/entity/LumberJackEntity.java
@@ -0,0 +1,11 @@
+package com.rimvillage.entity;
+
+import com.rimvillage.base.BaseVillagerEntity;
+
+public class LumberJackEntity extends BaseVillagerEntity {
+    
+    public LumberJackEntity() {
+        
+    }
+
+}
