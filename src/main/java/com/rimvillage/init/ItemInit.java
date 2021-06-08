f7ba76ef14751ea4fde06b5b006f2e6824b0db34
Bistard <1015813038@qq.com>
2021-06-08 23:52:47 +0800

    remove test_item_2

--- src/main/java/com/rimvillage/init/ItemInit.java
+++ src/main/java/com/rimvillage/init/ItemInit.java
@@ -13,11 +13,10 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RimVillageCore.MOD_ID);
     // Items
     public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item", () -> new Item(new Item.Properties().group(RimVillageCore.RIMVILLAGE_GROUP)));
-    public static final RegistryObject<Item> TEST_ITEM_2 = ITEMS.register("test_item_2", () -> new Item(new Item.Properties().group(RimVillageCore.RIMVILLAGE_GROUP)));
     public static final RegistryObject<Item> RIMVILLAGER_EGG = ITEMS.register("rimvillager_egg", () -> new Item(new Item.Properties().group(RimVillageCore.RIMVILLAGE_GROUP)));
 
     // Block Items
     public static final RegistryObject<BlockItem> RECRUIT_SIGN = ITEMS.register("recruit_sign", () -> new BlockItem(BlockInit.RECRUIT_SIGN.get(), new Item.Properties().group(RimVillageCore.RIMVILLAGE_GROUP)));
     public static final RegistryObject<BlockItem> VILLAGE_CENTER_SIGN = ITEMS.register("village_center_sign", () -> new BlockItem(BlockInit.VILLAGE_CENTER_SIGN.get(), new Item.Properties().group(RimVillageCore.RIMVILLAGE_GROUP)));
 
-    }
+}
