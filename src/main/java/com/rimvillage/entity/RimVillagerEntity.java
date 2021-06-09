e9c295cc20d096dcf86a2720f5df1606d4e97ef9
ZhaoguangHu <s2024256@de.ac.uk>
2021-06-09 13:02:05 +0800

    update the RimvillagerEntity.java file

--- src/main/java/com/rimvillage/entity/RimVillagerEntity.java
+++ src/main/java/com/rimvillage/entity/RimVillagerEntity.java
@@ -4,18 +4,34 @@ import com.rimvillage.base.BaseRimVillagerEntity;
 import com.rimvillage.util.ProfessionType;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.world.World;
 
 public class RimVillagerEntity extends BaseRimVillagerEntity {
 
-    private int miningLevel;            //miner
-    private int loggingLevel;           //Lumberjack
-    private int fishingLevel;           //Fisherman
-    private int breedingLevel;          //Rancher
-    private int farmingLevel;           //Farmer
-    private int manufacturingLevel;     //artisan
-    private int cookingLevel;           //chef
-    private int combatLevel;            //guard
+    private static final DataParameter<Integer> MININGLEVEL = EntityDataManager.createKey(RimVillagerEntity.class, DataSerializers.VARINT);
+    private static final DataParameter<Integer> LOGGINGLEVEL = EntityDataManager.createKey(RimVillagerEntity.class, DataSerializers.VARINT);
+    private static final DataParameter<Integer> FISHINGLEVEL = EntityDataManager.createKey(RimVillagerEntity.class, DataSerializers.VARINT);
+    private static final DataParameter<Integer> BREEDINGLEVEL = EntityDataManager.createKey(RimVillagerEntity.class, DataSerializers.VARINT);
+    private static final DataParameter<Integer> FARMINGLEVEL = EntityDataManager.createKey(RimVillagerEntity.class, DataSerializers.VARINT);
+    private static final DataParameter<Integer> MANUFACTURINGLEVEL = EntityDataManager.createKey(RimVillagerEntity.class, DataSerializers.VARINT);
+    private static final DataParameter<Integer> COOKINGLEVEL = EntityDataManager.createKey(RimVillagerEntity.class, DataSerializers.VARINT);
+    private static final DataParameter<Integer> COMBATLEVEL = EntityDataManager.createKey(RimVillagerEntity.class, DataSerializers.VARINT);
+
+    //register each skill level, initial them to 0 (initial value may changed in the future)
+    @Override
+    protected void registerData(){
+        this.dataManager.register(MININGLEVEL, 0);
+        this.dataManager.register(LOGGINGLEVEL, 0);
+        this.dataManager.register(FISHINGLEVEL, 0);
+        this.dataManager.register(BREEDINGLEVEL, 0);
+        this.dataManager.register(FARMINGLEVEL, 0);
+        this.dataManager.register(MANUFACTURINGLEVEL, 0);
+        this.dataManager.register(COOKINGLEVEL, 0);
+        this.dataManager.register(COMBATLEVEL, 0);
+    }
 
     public RimVillagerEntity(EntityType<? extends VillagerEntity> entityType, World worldIn, ProfessionType professionType, String name, Boolean isMale) {
         super(entityType, worldIn, name, isMale);
